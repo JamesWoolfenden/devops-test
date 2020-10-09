9ae1ba92b9314d297e45c791fb0b03ecf9f59527
James Woolfenden <james.woolfenden@slalom.com>
2020-10-09 18:37:56 +0100

    service

--- install.sh
+++ install.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
 sudo apt-get update && sudo apt-get -y upgrade
-sudo apt-get install nodejs -y
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt install nodejs -y
+echo ---------------------
+echo "NOW INSTALL SERVICE"
+echo ---------------------
+sudo cp index.service /etc/systemd/system/index.service
 sudo cp index.service /lib/systemd/system/index.service
+sudo chmod 644 /etc/systemd/system/index.service
+echo ---------------------
+echo "ENABLE SERVICE"
+echo ---------------------
 sudo systemctl daemon-reload
+echo ---------------------
+echo "Start SERVICE"
+echo ---------------------
+sudo systemctl start index
+sudo systemctl status myservice
+sudo systemd-analyze verify index.service
+echo ---------------------
+echo "Enable on reboot"
+echo ---------------------
 sudo systemctl enable index
