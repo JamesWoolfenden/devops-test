36df89a7d73c77ebde1ad13878b4f0ad06b95072
James Woolfenden <James.Woolfenden@gmail.com>
2020-10-09 18:44:01 +0100

    service file

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 sudo apt-get update && sudo apt-get -y upgrade
 sudo apt-get install nodejs -y
-sudo cp index.service /lib/systemd/system/index.service
 sudo cp index.service /etc/systemd/system/index.service
 sudo systemctl daemon-reload
 sudo systemctl enable index.service
