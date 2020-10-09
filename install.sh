82eb69331b5223a21cc195438028da7f0ab9e165
James Woolfenden <james.woolfenden@slalom.com>
2020-10-09 17:00:28 +0100

    systemd instead

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 sudo apt-get update && sudo apt-get -y upgrade
 sudo apt-get install nodejs -y
-sudo apt-get install npm -y
-npm install
 sudo cp index.service /lib/systemd/system/index.service
 sudo systemctl daemon-reload
-sudo systemctl start index
 sudo systemctl enable index
